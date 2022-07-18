cc4402ac3d7044daf8acf0b37bf64827a34a56d0
RDiMag <87457132+RDiMag@users.noreply.github.com>
2022-07-18 08:14:05 -0400

    CodeWars day 8

--- index.js
+++ index.js
@@ -96,3 +96,13 @@ function makeNegative(num) {
 
 // I used Math.sign to determine if the input number is negative or positive. If negative, it is returned as-is. If positive, it uses the -Math.abs
 // method to return a negative number.
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+function paperwork(n, m) {
+  if (n < 0 || m < 0) {
+    return 0;
+  } else {
+    return (m * n);
+  }
+}
