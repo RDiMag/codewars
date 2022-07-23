7b237bcf79194700fd2e1a1a4576f91f758fd056
RDiMag <87457132+RDiMag@users.noreply.github.com>
2022-07-22 21:08:40 -0400

    convert number to string

--- index.js
+++ index.js
@@ -114,3 +114,10 @@ function noSpace(x){
   let a = x.replace(/\s/g, '');
   return a;
 }
+
+// We need a function that can transform a number (integer) into a string.
+// What ways of achieving this do you know?
+
+function numberToString(num) {
+  return num.toString();
+}
