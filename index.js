bfeef7cbf7b05f3db8ab343cbb0289843ce06d9b
RDiMag <87457132+RDiMag@users.noreply.github.com>
2022-07-31 15:12:31 -0400

    codewars

--- index.js
+++ index.js
@@ -148,3 +148,16 @@ function boolToWord( bool ){
     return "No"
   }
 }
+
+// Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+// If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+
+// Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java, Scala and Julia), "$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell and Lua) or "¥" (Rust).
+
+function bonusTime(salary, bonus) {
+  if (bonus === true) {
+    salary = salary * 10;
+  }
+  return '\u00a3'+ salary.toString();
+}
