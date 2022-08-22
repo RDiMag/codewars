f278d1b9c4ef3b1dc149d75d8046439b02f39535
RDiMag <87457132+RDiMag@users.noreply.github.com>
2022-08-22 19:25:37 -0400

    codewars

--- index.js
+++ index.js
@@ -201,3 +201,13 @@ function sumMix(x){
 function twoSort(s) {
   return s.sort()[0].split('').join('***');
 }
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+// Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
+
+function check(a, x) {
+  if (a.includes(x)) {
+    return true;
+  } else return false;
+}
