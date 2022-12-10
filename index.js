d3d2e768bd79a10da17b0cc99bab37eb8eb764c2
RDiMag <87457132+RDiMag@users.noreply.github.com>
2022-12-09 21:10:46 -0500

    codewars

--- index.js
+++ index.js
@@ -275,3 +275,19 @@ function getAge(inputString){
   let n = (inputString).charAt(0);
   return Math.floor(n);
 }
+
+function invert(array) {
+  let invert = [];
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] > 0) {
+      invert.push(array[i] - (array[i] * 2));
+    }
+    if(array[i] < 0) {
+      invert.push(Math.abs(array[i]));
+    }
+    if (array[i] == 0) {
+      invert.push(-0);
+    }
+  }
+   return invert;
+}
