d9c63e3539ef0b8cf4ab03c12490f8d894be5e81
RDiMag <87457132+RDiMag@users.noreply.github.com>
2023-03-11 15:18:24 -0500

    codewars, basic math

--- index.js
+++ index.js
@@ -365,3 +365,20 @@ function solution(str){
 function countSheeps(arrayOfSheeps) {
   return arrayOfSheeps.filter(Boolean).length;
 }
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+// Examples(Operator, value1, value2) --> output
+
+function basicOp(operation, value1, value2)
+{
+  if(operation === '+'){
+    return value1 + value2
+  } else if (operation === '-'){
+    return value1 - value2
+  } else if (operation === '*'){
+    return value1 * value2
+  } else if (operation === '/'){
+    return value1 / value2
+  }
+}
